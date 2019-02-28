7186f79197a829b998f9f36fbc2b41c98765fb93
Karoly Kovacs <karoly.kovacs0920@gmail.com>
2019-02-28 21:13:39 +0100

    Updates

---
+++ precache-manifest.768e189da6b8b7a0274e0d6010d5d666.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c1685551921bf09af6a5639a91575917",
+    "url": "/static/media/background.c1685551.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "02c160bf7ec19632ff16",
+    "url": "/static/js/main.02c160bf.chunk.js"
+  },
+  {
+    "revision": "4a34e45d34cafb7623bd",
+    "url": "/static/js/1.4a34e45d.chunk.js"
+  },
+  {
+    "revision": "02c160bf7ec19632ff16",
+    "url": "/static/css/main.95fda695.chunk.css"
+  },
+  {
+    "revision": "6d2a41d0e18c33a0903ce8714ff90e0f",
+    "url": "/index.html"
+  }
+];
